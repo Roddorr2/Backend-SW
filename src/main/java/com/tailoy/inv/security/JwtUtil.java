5176b898a354833fa532240698c25ff3567575c7
Roddorr2 <rodricu0703@gmail.com>
2025-07-02 01:17:03 -0500

    Clase de utilidad para JWT

--- src/main/java/com/tailoy/inv/security/JwtUtil.java
+++ src/main/java/com/tailoy/inv/security/JwtUtil.java
@@ -1,4 +1,41 @@
 package com.tailoy.inv.security;
 
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.SignatureAlgorithm;
+@Component
 public class JwtUtil {
+    private final String jwtSecret = "nj8f+OwZtegrWTovLoUUcfq9BbaC9T9QyB7dIiqjybpRTMqizrdGDPmel2CYdgpV54CGlyyjqpglJI4E0i5knw==";
+    private final int jwtExpirationMs = 86400000;
+    private final Key key = Keys.hmacShaKeyFor(java.util.Base64.getDecoder().decode(jwtSecret));
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
 }
