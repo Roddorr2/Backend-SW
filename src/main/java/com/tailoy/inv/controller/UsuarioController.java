a3b69865ef7d16c5c964a347b95b001c7a129502
Roddorr2 <rodricu0703@gmail.com>
2025-07-02 02:06:24 -0500

    Controller de producto

--- src/main/java/com/tailoy/inv/controller/UsuarioController.java
+++ src/main/java/com/tailoy/inv/controller/UsuarioController.java
@@ -1,13 +1,8 @@
 package com.tailoy.inv.controller;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 import com.tailoy.inv.dto.UsuarioDTO;
 import com.tailoy.inv.model.Usuario;
