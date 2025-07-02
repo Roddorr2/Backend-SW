b679823df5bdd3f68ba11340e91b8f3f1e7f953c
Roddorr2 <rodricu0703@gmail.com>
2025-07-02 01:13:30 -0500

    Nuevo método para obtener correo

--- src/main/java/com/tailoy/inv/repository/UsuarioRepository.java
+++ src/main/java/com/tailoy/inv/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package com.tailoy.inv.repository;
 import com.tailoy.inv.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+import java.util.Optional;
 
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByNombre(String nombre);
+    Optional<Usuario> findByCorreo(String correo);
 }
